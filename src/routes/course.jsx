5e123570ab1c552224f0d97f7bb461d584650252
Giordano Alves <giordano@gridfinnance.ie>
2024-05-27 21:12:45 +0100

    Save video time elapsed
    * Add onPlayerPlay event to update the course log state from started to in_progress;
    * Get data from course log to set the start time of player;
    * Update the video time elapsed on pause event;
    * Add player ended event;
    * Add useRef to set the courseLog;

--- src/routes/course.jsx
+++ src/routes/course.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
@@ -8,24 +8,93 @@ import Typography from '@mui/material/Typography';
 
 
 export default function Course() {
-  const [course, setCourse] = useState([]);
-  const { courseId } = useParams()
-  const url = `http://localhost:3000/courses/${courseId}`;
-  const videoStartTime = 0;
+  const url                                 = "http://localhost:3000/";
+  const { courseId }                        = useParams()
+  const [course, setCourse]                 = useState([]);
+  const [videoStartTime, setVideoStartTime] = useState([]);
+  const courseLogRef                        = useRef(null);
 
-  useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setCourse(data);
-      });
-  }, [url]);
+  const fetchCourse = ()=> {
+    fetch(url + `courses/${courseId}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setCourse(data);
+    });
+  };
+
+
+  const createCourseLog = (kind)=> {
+    let data = { course_log: { kind: kind } }
+
+    fetch(url + `courses/${courseId}/course_logs`, {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      courseLogRef.current = data.course_log;
+      setVideoStartTime(data.course_log.video.time_elapsed_in_seconds);
+      console.log(videoStartTime)
+    });
+  };
+
+  const updateCourseLog = (state, time_elapsed_in_seconds)=> {
+    let data = {
+      course_log: {
+        state: state,
+        video_attributes: { time_elapsed_in_seconds: time_elapsed_in_seconds }
+      }
+    }
+
+    fetch(url + `courses/${courseId}/course_logs/${courseLogRef.current.id}`, {
+      method: 'put',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      courseLogRef.current = data.course_log;
+    });
+  };
+
+  const handlePlayerPlay = (player) => {
+    updateCourseLog('in_progress', player.currentTime());
+    console.log(`The learner has pressed play at ${player.currentTime()}`);
+  };
 
   const handlePlayerPause = (player) => {
+    updateCourseLog('in_progress', player.currentTime());
     console.log(`The learner has pressed pause at ${player.currentTime()}`);
   };
+
+  const handlePlayerEnded = (player) => {
+    updateCourseLog('finished', 0);
+    console.log(`The learner has ended ${player.currentTime()}`);
+  };
+
+  useEffect(() => {
+    fetchCourse();
+  }, []);
+
+  useEffect(() => {
+    createCourseLog('video');
+  }, []);
+
+  useEffect(() => {
+    console.log(`Video current time: ${videoStartTime}`)
+  },[videoStartTime]);
+
 
   return (
     <Box component="main" sx={{flexGrow: 1, height: '100vh', overflow: 'auto', padding: "5em" }}>
@@ -37,9 +106,15 @@ export default function Course() {
         <Typography gutterBottom variant="h6" component="div">
           {course.description}
         </Typography>
-        {course.video_url &&
-          <KubicleVideoPlayer videoUrl={course.video_url} startTime={videoStartTime} onPlayerPause={handlePlayerPause} />
-        }
+        {course.video_url && (
+          <KubicleVideoPlayer
+            videoUrl={course.video_url}
+            startTime={videoStartTime}
+            onPlayerPause={handlePlayerPause}
+            onPlayerPlay={handlePlayerPlay}
+            onPlayerEnded={handlePlayerEnded}
+          />
+        )}
       </Paper>
     </Box>
   );
